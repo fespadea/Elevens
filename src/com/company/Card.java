4c4df9e5dfd15a361eec0f735e680bd2fcacfcf8
fspadea <fs279@pvsd.org>
2017-03-16 14:26:38 -0400

    <starter code>

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -25,14 +25,15 @@ public class Card {
 	/**
 	 * Creates a new <code>Card</code> instance.
 	 *
-	 * @param cardRank       a <code>String</code> value
-	 *                       containing the rank of the card
-	 * @param cardSuit       a <code>String</code> value
-	 *                       containing the suit of the card
+	 * @param cardRank  a <code>String</code> value
+	 *                  containing the rank of the card
+	 * @param cardSuit  a <code>String</code> value
+	 *                  containing the suit of the card
 	 * @param cardPointValue an <code>int</code> value
-	 *                       containing the point value of the card
+	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
+		//initializes a new Card with the given rank, suit, and point value
 		rank = cardRank;
 		suit = cardSuit;
 		pointValue = cardPointValue;
@@ -41,7 +42,6 @@ public class Card {
 
 	/**
 	 * Accesses this <code>Card's</code> suit.
-	 *
 	 * @return this <code>Card's</code> suit.
 	 */
 	public String suit() {
@@ -50,7 +50,6 @@ public class Card {
 
 	/**
 	 * Accesses this <code>Card's</code> rank.
-	 *
 	 * @return this <code>Card's</code> rank.
 	 */
 	public String rank() {
@@ -59,37 +58,33 @@ public class Card {
 
 	/**
 	 * Accesses this <code>Card's</code> point value.
-	 *
 	 * @return this <code>Card's</code> point value.
 	 */
 	public int pointValue() {
 		return pointValue;
 	}
 
-	/**
-	 * Compare this card with the argument.
-	 *
+	/** Compare this card with the argument.
 	 * @param otherCard the other card to compare to this
 	 * @return true if the rank, suit, and point value of this card
-	 * are equal to those of the argument;
-	 * false otherwise.
+	 *              are equal to those of the argument;
+	 *         false otherwise.
 	 */
 	public boolean matches(Card otherCard) {
-		if(toString().equals(otherCard.toString())){
-			return true;
-		}
-		return false;
+		return otherCard.suit().equals(this.suit())
+				&& otherCard.rank().equals(this.rank())
+				&& otherCard.pointValue() == this.pointValue();
 	}
 
 	/**
 	 * Converts the rank, suit, and point value into a string in the format
-	 * "[Rank] of [Suit] (point value = [PointValue])".
+	 *     "[Rank] of [Suit] (point value = [PointValue])".
 	 * This provides a useful way of printing the contents
 	 * of a <code>Deck</code> in an easily readable format or performing
 	 * other similar functions.
 	 *
 	 * @return a <code>String</code> containing the rank, suit,
-	 * and point value of the card.
+	 *         and point value of the card.
 	 */
 	@Override
 	public String toString() {
